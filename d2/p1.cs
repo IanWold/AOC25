50893901a8d2e7e85083a4954761d3b813f49554
IanWold <ilwold@hotmail.com>
2025-12-02 11:06:02 -0600

    Add d2p2

---
+++ d2/p1.cs
@@ -0,0 +1,25 @@
+var ranges = Array.Empty<(long low, long high)>();
+using (var reader = new StreamReader("input")){
+    ranges = [..
+        reader.ReadToEnd().Split(",")
+        .Select(r => r.Split('-'))
+        .Select(r => (Convert.ToInt64(r[0]), Convert.ToInt64(r[1])))
+    ];
+}
+
+long total = 0;
+
+foreach (var range in ranges) {
+    for (long idNumber = range.low; idNumber <= range.high; idNumber++) {
+        var id = idNumber.ToString();
+        if (id.Length % 2 != 0) {
+            continue;
+        }
+
+        if (id.Substring(0, id.Length/2) == id.Substring(id.Length/2)) {
+            total += idNumber;
+        }
+    }
+}
+
+Console.WriteLine(total);
